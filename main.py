73cf65b536d4f5d7320826d3e99bd9c8983066e6
tenz0wo <igor_rom7011@bk.ru>
2023-01-15 22:31:47 +0300

    dump selection

--- main.py
+++ main.py
@@ -6,27 +6,89 @@ import os
 y = yadisk.YaDisk(token="y0_AgAAAAAiAPOmAAkAuwAAAADZsTiE4v3jZZT3QaOPZ4FwYty79atKgEs")
 
 
-def uploadDisk():
+def menu():
+    mFlag = input("""Hey, I am a bot for filling the database in json file format of the form:
+    - Proposal to be answered
+    - Answer
+    good cat scribblers <3 meows
+    to start, enter one of the commands
+    [*] grabbler, [*] clear (dev)\n
+    """)
+    check(mFlag)
+
+def check(mFlag):
+    if mFlag == "grabbler":
+        grab()
+    else:
+        mFlag = input("""
+    [-] Error…
+    to start, enter one of the commands
+    [*] grabbler, [*] clear (dev)\n
+    """)
+        check(mFlag)
+        
+
+def loadall(dir):
     now = datetime.datetime.now()
     directory = './json'
-    if y.check_token():
-        if not y.is_dir(f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}"):
-            y.mkdir(f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}")
-            print(f"[+] Dir /json/{now.day}.{now.month}.{now.year}.{now.microsecond} create")
+
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         if os.path.isfile(f):
-            y.upload(f"json/{filename}", f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}/{filename}")
-            print(f"[+] File {now.day}.{now.month}.{now.year}.{now.microsecond}/{filename} create")
+            y.upload(f"json/{filename}", f"{dir}/{filename}")
+            print(f"[+] File {now.day}.{now.month}.{now.year}.{now.microsecond}/{filename} load")
+        else:
+            print(f"[-] File {now.day}.{now.month}.{now.year}.{now.microsecond}/{filename} already loaded")
+    print("[+] Succsess!")
+
+def loadone(minname, dir):
+    directory = './json'
+
+    f = os.path.join(directory, minname)
+    if os.path.isfile(f):
+        y.upload(f"json/{minname}", f"{dir}/{minname}")
+        print(f"[+] File {dir}/{minname} load")
+    else:
+        print(f"[-] File {dir}/{minname} already loaded")
+    print("[+] Succsess!")
+
+
+def flag(minname, dir):
+    flag = input("Do you want to upload only last file? if yes then enter Y, otherwise n. Y/n: ")
+    if flag == "Y":
+        loadone(minname, dir)
+    elif flag == "n":
+        loadall(dir)
+    else:
+        print("[-] Error…")
+        print("[?] try again…")
+        uploadDisk(minname)
+
+def uploadDisk(minname):
+    now = datetime.datetime.now()
+    print("|__________dump to json folder on yadisk__________|\n")
+    if y.check_token():
+        if not y.is_dir(f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}"):
+            y.mkdir(f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}")
+            dir = (f"/json/{now.day}.{now.month}.{now.year}.{now.microsecond}")
+            print(f"[+] Dir /json/{now.day}.{now.month}.{now.year}.{now.microsecond} create\n")
+        else:
+            print(f"[-] Dir /json/{now.day}.{now.month}.{now.year}.{now.microsecond} already created\n")
+            print("[?] try again…")
+            uploadDisk()
+    flag(minname, dir)
 
 def save(messages):
     now = datetime.datetime.now()
     name = f"json/dump{now.microsecond}.json"
+    minname = f"dump{now.microsecond}.json"
     with open(name, 'a') as file:
         json.dump(messages, file, indent=0, ensure_ascii=False)
         file.write('\n') 
-    uploadDisk()
-
+        print("|_______________dump to json folder________________|\n")
+        print("")
+        print(f"[+] File {name} save\n")
+    uploadDisk(minname)
 
 def grab():
     messages = []
@@ -34,8 +96,8 @@ def grab():
     i = 0
 
     while True:
-        question = input('Предложение на которое надо ответить: ')
-        answer =  input('ОтВет: ')
+        question = input('Proposal to be answered: ')
+        answer =  input('Answer: ')
         messages.append({
             "question": question,
             "answer": answer
@@ -45,5 +107,6 @@ def grab():
         if i == n:
             save(messages)
             break
-            
-grab()
+
+if __name__ == "__main__":    
+    menu()
